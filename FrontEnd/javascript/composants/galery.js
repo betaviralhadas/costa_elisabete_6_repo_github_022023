e2b5e8535d339d207c64c116ec415c17f0fcded6
elisabete <beta_viralhadas@hotmail.com>
2023-04-01 23:37:44 +0200

    modal js afficher et suprimer les travaux

--- FrontEnd/javascript/composants/galery.js
+++ FrontEnd/javascript/composants/galery.js
@@ -1,65 +1,59 @@
-/*retourner la list de la galerie fournie par l api*/
-const galerie = async () => {  //funcao
-    const listgalerie = await fetch("http://localhost:5678/api/works", { //fetch interage com a api
+
+const galerie = async () => {
+    const listgalerie = await fetch("http://localhost:5678/api/works", {
         method: "GET",
         headers: {
             'Content-Type': 'application/json'
         }
     })
-        .then((res) => res.json()); //then é 1promessa
-    return listgalerie; //retourne la list des travaux a la fin
+        .then((res) => res.json());
+    return listgalerie;
 }
 
 const gallery = document.querySelector(".gallery");
-/*afficher les travaux sur la page d accueil*/
+
 const affichework = async () => {
-    const work = await galerie(); // xamei a funcao galerie, await attend la reponse d la list des travaux
+    const work = await galerie();
 
     for (let i = 0; i < work.length; i++) {
-        let figure = document.createElement("figure");//crear a tag figure
-        gallery.appendChild(figure);//o parente gallery xama o filho figure
+        let figure = document.createElement("figure");
+        gallery.appendChild(figure);
         let image = document.createElement("img");
         figure.appendChild(image);
         image.src = work[i].imageUrl; //fui buscar o valor imageurl ao ficheiro workmodel do back
-        let figcaption = document.createElement("figcaption"); //o k ta no parenteses é a tag do html
+        let figcaption = document.createElement("figcaption");
         figure.appendChild(figcaption);
         figure.classList.add("category" + work[i].category.id);
         figure.classList.add("all");
         figcaption.innerText = work[i].title;
     }
 }
-window.onload = affichework();//desde k o site e aberto ele executa a funcao affichework (para mostrar as imgs e texto)
+window.onload = affichework();
 
 
-//let gallery = document.querySelector(".gallery");
 const filtrer = async () => {
     const category = await fetch("http://localhost:5678/api" + "/categories", {
         method: "GET",
         headers: {
             "content-type": "application/json",
         }
-
     })
         .then((res) => res.json());
     return category;
 }
-//const selectionfilters = document.querySelector(".selectionfilters");
+
 const affichecategory = async (elt_category) => {
     const category = await filtrer();
 
-    //creer le bouton "tous" en dehors de boucle
     const creerdiv = document.createElement("article");
-    //const button = document.createElement("button");
     creerdiv.classList.add("selectionfilters");
     const btn_all = document.createElement("button");
     btn_all.innerHTML = "Tous";
     btn_all.classList.add("filters");
     btn_all.dataset.category = "all";
     creerdiv.appendChild(btn_all);
-    //pour cibler les boutons
     btn_all.setAttribute("data-name", "Tous");
-
-    //creer les 3 boutons de filtre
+    
     for (let i = 0; i < category.length; i++) {
         const btn = document.createElement("button");
         btn.innerHTML = category[i].name;
@@ -69,15 +63,13 @@ const affichecategory = async (elt_category) => {
         if (i === 1 || i === 2) {
             btn.classList.add("largeurfilter4");
         }
-        //btn.setAttribute("data-name", category[i].name);
     }
     for (const button of creerdiv.children) {
         button.addEventListener('click', () => {
             afficheCategoryFiltres(button.dataset.category)
         })
     }
-    //inserer cote a cote avant gallery
-    gallery.insertAdjacentElement("beforebegin", creerdiv);
+    gallery.insertAdjacentElement("beforebegin", creerdiv); //inserer cote a cote avant gallery
 }
 affichecategory();
 
@@ -104,28 +96,3 @@ else {
         elementsmasque.style.display = "none"
     })
 }
-
-
-/*
-const selectionfilters = document.querySelector(".selectionfilters");
-fetch("http://localhost:5678/api/categories")
-.then((res)=> res.json())
-.then(function(data){// é uma promessa com uma funcao k vai pegar o resultado da primeira promessa
-    for(let i;i<data.length;i++){
-        console.log(data.length);
-        let button = document.createElement("button");
-        selectionfilters.appendChild(button);
-        button.classList.add("filters"); // para dar class á tag button
-        button.innerText = data[i].name; //para afichar o nome da categoria k se encontra na posicao i e name fui buscar ao dossier categories model
-        button.dataset.category="category"+data[i].id;
-    }
-    for(let buton of selectionfilters.children){
-
-    }
-    //gallery.insertAdjacentElement("beforebegin",button);
-})
-
-const filtre = async () => {
-
-}
-*/
--- FrontEnd/javascript/modal.js
+++ FrontEnd/javascript/modal.js
@@ -52,31 +52,36 @@ const afficheworkmodal = async () => {
     for (let i = 0; i < work.length; i++) {
         let figure = document.createElement("figure");//crear a tag figure
         gallerymodal.appendChild(figure);//o parente gallery xama o filho figure
+        figure.classList.add("category" + work[i].category.id);
+        figure.classList.add("all");
+        figure.classList.add("modal_cart");
         let image = document.createElement("img");
         figure.appendChild(image);
         image.src = work[i].imageUrl; //fui buscar o valor imageurl ao ficheiro workmodel do back
         let editer = document.createElement("p");
         figure.appendChild(editer);
         editer.innerHTML = "éditer";
+        editer.classList.add("editer");
         let figcaption = document.createElement("figcaption");
         figure.appendChild(figcaption);
+        figcaption.classList.add("icons");
         figcaption.innerHTML = `<i class = "fa-solid fa-trash-can"></i>`;
         figcaption.addEventListener('click', deleteImg.bind(this,work[i].id));
-        //let icon = document.createElement("i");
-        //figure.appendChild(icon);
-        figure.classList.add("category" + work[i].category.id);
-        figure.classList.add("all");
-        editer.classList.add("editer");
-
-        //icon.classList.add("fa-solid fa-trash-can");
+        
         if (i=== 0){
             figcaption.innerHTML = `<i class="fa-solid fa-arrows-up-down-left-right"></i> <i class = "fa-solid fa-trash-can"></i>`;
         }
     }
 }
 
-const tokenuser = localStorage.getItem("token");
+window.addEventListener('keydown', function (e) {
+    if (e.key === "Escape" || e.key === "Esc") {
+        closeModal(e)
+    }
+})
 
+const tokenuser = localStorage.getItem("token");
+//funcao para apagar as fotos
 const deleteImg = async (id) => {
     const listgalerie = await fetch("http://localhost:5678/api/works/{id}", {
         method: "DELETE",
@@ -96,17 +101,102 @@ const deleteImg = async (id) => {
         });
     return listgalerie;
 }
-/*<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><!--! Font Awesome Pro 6.3.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. --><path d="M170.5 51.6L151.5 80h145l-19-28.4c-1.5-2.2-4-3.6-6.7-3.6H177.1c-2.7 0-5.2 1.3-6.7 3.6zm147-26.6L354.2 80H368h48 8c13.3 0 24 10.7 24 24s-10.7 24-24 24h-8V432c0 44.2-35.8 80-80 80H112c-44.2 0-80-35.8-80-80V128H24c-13.3 0-24-10.7-24-24S10.7 80 24 80h8H80 93.8l36.7-55.1C140.9 9.4 158.4 0 177.1 0h93.7c18.7 0 36.2 9.4 46.6 24.9zM80 128V432c0 17.7 14.3 32 32 32H336c17.7 0 32-14.3 32-32V128H80zm80 64V400c0 8.8-7.2 16-16 16s-16-7.2-16-16V192c0-8.8 7.2-16 16-16s16 7.2 16 16zm80 0V400c0 8.8-7.2 16-16 16s-16-7.2-16-16V192c0-8.8 7.2-16 16-16s16 7.2 16 16zm80 0V400c0 8.8-7.2 16-16 16s-16-7.2-16-16V192c0-8.8 7.2-16 16-16s16 7.2 16 16z"/></svg>
-para selecionar todos os elementos, 
-metodo foreach -para cada um dos elementos vamos escutar o clic, 
-entre parenteses eu vou selecionar cada lien
-para cada lien vou juntar 1event e quando vamos clicar no lien vamos chamar uma funcao k vai se xamar openmodal
-depois criar a funcao numa constante, é a funcao k vai ter os parametros do evento
-/* const modal = document.querySelector(e.modal.getAttribute('href'))
-    modal.style.display = null
-    modal.removeAttribute ('aria-hidden')
-    modal.setAttribute('aria-modal', 'true')
-    modal = modal
-    modal.addEventListener('click', closeModal)
-    modal.querySelector('.jsclosemodal').addEventListener('click', closeModal)
+
+
+const modal1 = document.getElementById("modal")
+//fermer en cliquant n'importe ou
+window.addEventListener("click", (e) => {
+    if (e.target == modal1) {
+        closeModal();
+    }
+});
+// funcao para adicionar uma foto no 2~modal
+/*const addImg = async => {
+    const listgalerie = await fetch("http://localhost:5678/api/works/post", {
+        method: "POST",
+        headers: {
+            'Content-Type': 'application/json',
+            
+        }
+    })
+    .then((res) => res.json());
+    return listgalerie;
+}
+*/
+
+/*
+const fileInput = document.querySelector("#fileInput");
+
+const uploadFile = file => {
+  console.log("Uploading file...");
+  const API_ENDPOINT = "https://file.io";
+  const request = new XMLHttpRequest();
+  const formData = new FormData();
+
+  request.open("POST", API_ENDPOINT, true);
+  request.onreadystatechange = () => {
+    if (request.readyState === 4 && request.status === 200) {
+      console.log(request.responseText);
+    }
+  };
+  formData.append("file", file);
+  request.send(formData);
+};
+
+fileInput.addEventListener("change", event => {
+  const files = event.target.files;
+  uploadFile(files[0]);
+});
+*/
+
+
+
+const inputFile = document.querySelector("#fileInput");
+const pictureImage = document.querySelector(".picture__image");
+const pictureImageTxt = "+ Ajouter photo";
+pictureImage.innerHTML = pictureImageTxt;
+
+inputFile.addEventListener("change", function (e) {
+  const inputTarget = e.target;
+  const file = inputTarget.files[0];
+
+  if (file) {
+    const reader = new FileReader();
+
+    reader.addEventListener("load", function (e) {
+      const readerTarget = e.target;
+
+      const img = document.createElement("img");
+      img.src = readerTarget.result;
+      img.classList.add("picture__img");
+
+      pictureImage.innerHTML = "";
+      pictureImage.appendChild(img);
+    });
+
+    reader.readAsDataURL(file);
+  } else {
+    pictureImage.innerHTML = pictureImageTxt;
+  }
+});
+
+
+/*
+const ajouterPhoto = async () => {
+    const listgalerie = await fetch("http://localhost:5678/api/works/post", {
+        method: "POST",
+        headers: {
+            'Content-Type': 'application/json',
+        }
+    })
+        .then((res) => {
+            if 
+            }
+            else {
+                openModal();
+                
+            }
+        });
+    return listgalerie;
+}
 */
